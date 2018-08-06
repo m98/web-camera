f6c4ed1435af06bacfb2f7ac107cfc556c53c299
Mohammad Kermani <98kermani@gmail.com>
2018-08-06 22:59:56 +0430

    fix(web-camera): use main function in

--- src/web-camera.ts
+++ src/web-camera.ts
@@ -1,4 +1,27 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export default class DummyClass {}
+import {Browser} from './browser';
+import {Dom} from './dom';
+
+export default class WebCamera {
+  static browser = new Browser();
+  static dom = new Dom();
+
+  constructor() {
+    // codes here
+  }
+
+  static getAccess() {
+    // codes here
+  }
+
+  static test() {
+    this.browser.getCameras((data, error) => {
+      if (data) {
+        this.dom.addCameraToPage();
+      }
+    });
+  }
+}
+
